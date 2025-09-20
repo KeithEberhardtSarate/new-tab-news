4883ec5c2a10f086e766a8fd21eff108137e4579
keith eberhardt sarate <keithsarate.info@gmail.com>
2025-09-20 21:03:20 +0000

    Make`database.js` client connect() more robust to errors

--- infra/database.js
+++ infra/database.js
@@ -1,16 +1,20 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
-  const client = new Client({
+  const postegresCredentials = {
     host: process.env.POSTGRES_HOST,
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-  });
-  await client.connect();
+  };
+
+  const client = new Client(postegresCredentials);
+
+  console.log("Postrgres credentials", postegresCredentials);
 
   try {
+    await client.connect();
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
